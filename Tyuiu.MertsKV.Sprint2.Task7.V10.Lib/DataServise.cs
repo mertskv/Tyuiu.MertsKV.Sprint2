779a0df350a33b44cb157e3094927dae048a76e6
mertskv <kmerts2@yandex.ru>
2025-10-16 21:25:07 +0500

    Update DataServise.cs

--- Tyuiu.MertsKV.Sprint2.Task7.V10.Lib/DataServise.cs
+++ Tyuiu.MertsKV.Sprint2.Task7.V10.Lib/DataServise.cs
@@ -7,15 +7,15 @@ namespace Tyuiu.MertsKV.Sprint2.Task7.V10.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && ((x > 0) && (x <= 1) && ((y > 0) && (x <= 1))))
+            if ((y >= x * x - 2) && (y >= 0 && y <= x) && (y <= 0 && y >= -x))
             {
                 res = true;
             }
-            else
+           else
             {
                 res = false;
             }
-            return res;
+                return res;
         }
     }
 }
