0f0fd6d573d9d8dd3413a0e39df18c3ef42d08ab
mertskv <kmerts2@yandex.ru>
2025-10-16 21:27:10 +0500

    Update DataServiseTest.cs

--- Tyuiu.MertsKV.Sprint2.Task7.V10.Test/DataServiseTest.cs
+++ Tyuiu.MertsKV.Sprint2.Task7.V10.Test/DataServiseTest.cs
@@ -9,10 +9,13 @@ namespace Tyuiu.MertsKV.Sprint2.Task7.V10.Test
         public void ValidCheckDotInShadedArea()
         {
             DataServise ds = new DataServise();
-            double x = 0.7;
-            double y = 0.5;
-            bool wait = true;
+            double x = 1;
+            double y = 0;
+            
             bool res = ds.CheckDotInShadedArea(x, y);
+            
+            bool wait = true;
+            
             Assert.AreEqual(wait, res);
         }
     
