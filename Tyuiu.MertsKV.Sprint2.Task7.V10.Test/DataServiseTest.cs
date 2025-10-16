d46e72cb80aca2f14969bde7f2e0318028dfa64f
mertskv <kmerts2@yandex.ru>
2025-10-16 22:00:26 +0500

    Update DataServiseTest.cs

--- Tyuiu.MertsKV.Sprint2.Task7.V10.Test/DataServiseTest.cs
+++ Tyuiu.MertsKV.Sprint2.Task7.V10.Test/DataServiseTest.cs
@@ -3,7 +3,7 @@ using Tyuiu.MertsKV.Sprint2.Task7.V10.Lib;
 namespace Tyuiu.MertsKV.Sprint2.Task7.V10.Test
 {
     [TestClass]
-    public sealed class DataServiseTest
+    public class DataServiseTest
     {
         [TestMethod]
         public void ValidCheckDotInShadedArea()
